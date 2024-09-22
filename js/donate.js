a61900dc82938376e9d4ef28ca52007aeb32675b
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-23 04:23:52 +0600

    added validation for section1

--- js/donate.js
+++ js/donate.js
@@ -1,6 +1,22 @@
 
 document.getElementById('donate-btn-1').addEventListener('click',function(){
-    document.getElementById('balance-01').textContent = donationForNoakhali().toFixed(2);
-    document.getElementById('account-balance').textContent = remainingBalanceNoakhali().toFixed(2);
-    
+
+    if(getInputFieldById('donation-noakhali') < 0 || isNaN(getInputFieldById('donation-noakhali')) ){
+        document.getElementById('amount-error-1').classList.remove('hidden')
+    }
+    else{
+        document.getElementById('balance-01').textContent = donationForNoakhali().toFixed(2);
+        document.getElementById('account-balance').textContent = remainingBalance('donation-noakhali').toFixed(2);
+        // alert('Thanks for your donation');
+        document.getElementById('amount-error-1').classList.add('hidden');
+        document.getElementById('my_modal_1').showModal();
+         addHistory('donation-noakhali');
+
+    }
+
+})
+
+document.getElementById('donate-btn-2').addEventListener('click',function(){
+    document.getElementById('balance-02').textContent = donationForFeni().toFixed(2);
+    console.log('money')
 })
--- js/utilities.js
+++ js/utilities.js
@@ -15,9 +15,24 @@ function donationForNoakhali(){
     return output
 }
 
-function remainingBalanceNoakhali(){
-    const input = getInputFieldById('donation-noakhali');
+
+
+
+
+function remainingBalance(id){
+    const input = getInputFieldById(id);
     const account = getInputTextById('account-balance');
     const balance = account-input;
     return balance;
 }
+
+function addHistory(id){
+    const div = document.createElement('div');
+    div.classList.add('bg-white','p-3','border','rounded-xl')
+    div.innerHTML = `
+    <p class=" font-semibold">${ getInputFieldById(id)} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+    <p class ="text-gray-500">Date:${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}</p>
+    `
+    document.getElementById('history').prepend(div);
+}
+
