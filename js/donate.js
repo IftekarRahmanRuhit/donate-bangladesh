70310514f0b36ef5229ab70fb8fe366bd69b2353
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-23 05:08:54 +0600

    added donation section2 and section3

--- js/donate.js
+++ js/donate.js
@@ -10,13 +10,44 @@ document.getElementById('donate-btn-1').addEventListener('click',function(){
         // alert('Thanks for your donation');
         document.getElementById('amount-error-1').classList.add('hidden');
         document.getElementById('my_modal_1').showModal();
-         addHistory('donation-noakhali');
+         addHistoryNoakhali();
 
     }
 
 })
 
 document.getElementById('donate-btn-2').addEventListener('click',function(){
-    document.getElementById('balance-02').textContent = donationForFeni().toFixed(2);
-    console.log('money')
+
+    if(getInputFieldById('donation-feni') < 0 || isNaN(getInputFieldById('donation-feni')) ){
+        document.getElementById('amount-error-2').classList.remove('hidden');
+    }
+    else{
+        document.getElementById('balance-02').textContent = donationForFeni().toFixed(2);
+        document.getElementById('account-balance').textContent = remainingBalance('donation-feni').toFixed(2);
+        // alert('Thanks for your donation');
+        document.getElementById('amount-error-2').classList.add('hidden');
+        document.getElementById('my_modal_1').showModal();
+
+        addHistoryFeni();
+
+    }
+
+})
+
+document.getElementById('donate-btn-3').addEventListener('click',function(){
+
+    if(getInputFieldById('donation-aid') < 0 || isNaN(getInputFieldById('donation-aid')) ){
+        document.getElementById('amount-error-3').classList.remove('hidden');
+    }
+    else{
+        document.getElementById('balance-03').textContent = donationForQuotaMovement().toFixed(2);
+        document.getElementById('account-balance').textContent = remainingBalance('donation-aid').toFixed(2);
+        // alert('Thanks for your donation');
+        document.getElementById('amount-error-3').classList.add('hidden');
+        document.getElementById('my_modal_1').showModal();
+
+        addHistoryQuotaMovement();
+
+    }
+
 })
--- js/utilities.js
+++ js/utilities.js
@@ -15,7 +15,19 @@ function donationForNoakhali(){
     return output
 }
 
-
+function donationForFeni(){
+    const input = getInputFieldById('donation-feni');
+    const account = getInputTextById('balance-02');
+    const output = input + account;
+    return output;
+}
+function donationForQuotaMovement(){
+    const input = getInputFieldById('donation-aid');
+    const account = getInputTextById('balance-03');
+    const output = input + account;
+    console.log(input,account,output);
+    return output;
+}
 
 
 
@@ -26,12 +38,32 @@ function remainingBalance(id){
     return balance;
 }
 
-function addHistory(id){
+function addHistoryNoakhali(){
     const div = document.createElement('div');
     div.classList.add('bg-white','p-3','border','rounded-xl')
     div.innerHTML = `
-    <p class=" font-semibold">${ getInputFieldById(id)} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-    <p class ="text-gray-500">Date:${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}</p>
+    <p class=" font-semibold">${ getInputFieldById('donation-noakhali')} Taka is Donated for famine-2024 at Noakhali, Bangladesh</p>
+    <p class ="text-gray-500 mt-1">Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} (Bangladesh Standard Time)</p>
+    `
+    document.getElementById('history').prepend(div);
+}
+
+function addHistoryFeni(){
+    const div = document.createElement('div');
+    div.classList.add('bg-white','p-3','border','rounded-xl')
+    div.innerHTML = `
+    <p class=" font-semibold">${ getInputFieldById('donation-feni')} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+    <p class ="text-gray-500 mt-1">Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} (Bangladesh Standard Time)</p>
+    `
+    document.getElementById('history').prepend(div);
+}
+
+function addHistoryQuotaMovement(){
+    const div = document.createElement('div');
+    div.classList.add('bg-white','p-3','border','rounded-xl')
+    div.innerHTML = `
+    <p class=" font-semibold">${ getInputFieldById('donation-aid')} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+    <p class ="text-gray-500 mt-1">Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} (Bangladesh Standard Time)</p>
     `
     document.getElementById('history').prepend(div);
 }
