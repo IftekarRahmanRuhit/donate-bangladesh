912d16647101133f7cde6ff147d09087dfc10dfa
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-23 21:14:55 +0600

    added alert function for invalid amount

--- js/donate.js
+++ js/donate.js
@@ -2,13 +2,15 @@
 document.getElementById('donate-btn-1').addEventListener('click',function(){
 
     if(getInputFieldById('donation-noakhali') <= 0 || isNaN(getInputFieldById('donation-noakhali'))){
-        document.getElementById('amount-error-1').classList.remove('hidden')
+        document.getElementById('amount-error-1').classList.remove('hidden');
+        alert('Invalid amount! Please enter a positive amount');
     }
     else{
         document.getElementById('balance-01').textContent = donationForNoakhali().toFixed(2);
         document.getElementById('account-balance').textContent = remainingBalance('donation-noakhali').toFixed(2);
         // alert('Thanks for your donation');
         document.getElementById('amount-error-1').classList.add('hidden');
+        
         document.getElementById('my_modal_1').showModal();
          addHistoryNoakhali();
 
@@ -20,6 +22,7 @@ document.getElementById('donate-btn-2').addEventListener('click',function(){
 
     if(getInputFieldById('donation-feni') <= 0 || isNaN(getInputFieldById('donation-feni'))){
         document.getElementById('amount-error-2').classList.remove('hidden');
+        alert('Invalid amount! Please enter a positive amount');
     }
     else{
         document.getElementById('balance-02').textContent = donationForFeni().toFixed(2);
@@ -38,6 +41,7 @@ document.getElementById('donate-btn-3').addEventListener('click',function(){
 
     if(getInputFieldById('donation-aid') <= 0 || isNaN(getInputFieldById('donation-aid'))){
         document.getElementById('amount-error-3').classList.remove('hidden');
+        alert('Invalid amount! Please enter a positive amount');
     }
     else{
         document.getElementById('balance-03').textContent = donationForQuotaMovement().toFixed(2);
